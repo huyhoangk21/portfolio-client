4c3de0155db48b895d78bb3d9e9f36eb446fd32e
Hoang Le <kuhpro@gmail.com>
2020-11-19 13:50:36 -0600

    add navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import { FaAlignJustify } from 'react-icons/fa'
 import './styles/navbar.scss'
 
 const Navbar = () => {
-  return <div className='navbar'></div>
+  const [open, setOpen] = useState(false)
+
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='logo' />
+        <div className='nav-links'>
+          <ul>{renderedNavLinks}</ul>
+        </div>
+        <button
+          type='button'
+          className='toggle-btn'
+          onClick={() => setOpen(!open)}
+        >
+          <FaAlignJustify />
+        </button>
+      </div>
+    </nav>
+  )
 }
+
+const links = [
+  { id: 1, url: '/', name: 'Home' },
+  { id: 2, url: '/resume', name: 'Resume' },
+  { id: 3, url: '/projects', name: 'Projects' },
+  { id: 4, url: '/blogs', name: 'Blog' },
+  { id: 5, url: '/contact', name: 'Contact' },
+]
+
+const renderedNavLinks = links.map(({ id, url, name }) => (
+  <li>
+    <Link key={id} to={url}>
+      {name}
+    </Link>
+  </li>
+))
 
 export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Navbar from '../components/navbar'
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' />
-    <div>Home</div>
     <Navbar />
-    <h1>Hello</h1>
-    <Link to='/404'>Click me</Link>
   </Layout>
 )
 
