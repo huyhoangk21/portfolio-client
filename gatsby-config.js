e44730fd6bf4a06ba22ed5ac9692e0cd44eac02c
Hoang Le <kuhpro@gmail.com>
2020-11-18 23:00:15 -0600

    initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Huy Hoang`,
+    description: `This is my portfolio website`,
+    author: `@huyhoang`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        data: `@import "${__dirname}/src/styles/styles";`,
+      },
+    },
   ],
 }
